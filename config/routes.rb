dded5f995a5322ae025d0873e0083366318f1ae2
Johnny Chen <johnnyc1314@gmail.com>
2022-06-06 15:09:57 -0400

    commit tree 2f60bf04efcbdc919f1a4eae2aeddb3668d8fe5d

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "spices", to: "spices#index"
+  # get "spices/:id", to: "spices#show"
+  post "spices", to: "spices#create"
+  patch "spices/:id", to: "spices#update"
+  delete "spices/:id", to: "spices#delete"
+
 end
